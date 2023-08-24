339b19a3b1e318ff2baed9a40470af3473725bf1
leandro.morais <leandro.morais@fengbrasil.com.br>
2023-08-24 13:55:50 -0300

    components - layout created

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,26 @@
-import React from 'react';
+import React from "react";
 import { Card } from "./components/Card";
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
-    <>
-    <Header />
+    <Layout>
       <div>
-        <Card 
-        id={1}
-        paragraph='Typescript'
-        details='TS para frontend e backend'
+        <Card
+          id={1}
+          paragraph="Typescript"
+          details="TS para frontend e backend"
         />
-        <Card 
-        id={2}
-        paragraph='HTML'
-        details='HTML para frontend'
-        />
-        <Card 
-        id={3}
-        paragraph='SQL'
-        details='SQL para banco de dados'
-        />
-        <Card 
-        id={4}
-        paragraph='Python'
-        details='Python para analise de dados'
+        <Card id={2} paragraph="HTML" details="HTML para frontend" />
+        <Card id={3} paragraph="SQL" details="SQL para banco de dados" />
+        <Card
+          id={4}
+          paragraph="Python"
+          details="Python para analise de dados"
         />
       </div>
-      <div>
-        Footer
-      </div>
-      <Footer />
-    </>
+      <div>Footer</div>
+    </Layout>
   );
 }
 
