7828231e8f79b04c09f9f14be8f99e21be6d0e7b
leandro.morais <leandro.morais@fengbrasil.com.br>
2023-08-24 13:47:24 -0300

    components header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import { Card } from "./components/Card";
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <>
+    <Header />
       <div>
-        Hello World!
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card 
+        id={1}
+        paragraph='Typescript'
+        details='TS para frontend e backend'
+        />
+        <Card 
+        id={2}
+        paragraph='HTML'
+        details='HTML para frontend'
+        />
+        <Card 
+        id={3}
+        paragraph='SQL'
+        details='SQL para banco de dados'
+        />
+        <Card 
+        id={4}
+        paragraph='Python'
+        details='Python para analise de dados'
+        />
       </div>
       <div>
         Footer
       </div>
+      <Footer />
     </>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,16 @@
-export const Card = () => {
+interface ICard {
+  id: number,
+  paragraph: string,
+  details: string
+}
+
+export const Card = ({ id, paragraph, details } : ICard) => {
+  console.log(id)
   return(
     <div>
-      <h1>Card</h1>
-      <p>Paragrafo</p>
-      <p>Details about cards</p>
+      <h1>Card {id}</h1>
+      <p>{paragraph}</p>
+      <p>{details}</p>
     </div>
   )
 }
